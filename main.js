afb7f246548f8db228f60dcf219650f717eb45bd
Mannex Pimentel <Mannex@Mannexs-MacBook-Air.local>
2018-10-11 16:53:53 -0400

    correct answer functionality

---
+++ main.js
@@ -0,0 +1,10 @@
+let q1 = document.querySelector('#q1')
+let q2 = document.querySelector('#q2')
+let q3 = document.querySelector('#q3')
+let score = 0
+
+q1.addEventListener('click', function(answer){
+    let newScore = score + 10
+    console.log(newScore) 
+})
+
